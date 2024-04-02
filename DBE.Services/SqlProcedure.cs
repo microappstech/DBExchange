560bcfd6746c7194afec9e48f77cdc39874d2831
hamza mouddakir <hamzamouddakur@gmail.com>
2024-04-02 10:25:53 +0000

    Test 1

---
+++ DBE.Services/SqlProcedure.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Procedures
+{
+    public static class SqlProcedure
+    {
+        private static Hashtable p = new Hashtable();
+        public static Hashtable Procedures
+        {
+            get { return p; }
+        }
+        static SqlProcedure()
+        {
+            #region Load Databases 
+            p["LoadDatabases"] = "SELECT name FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');";
+            #endregion
+        }
+    }
+}
